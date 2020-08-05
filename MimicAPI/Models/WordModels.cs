1f9003c5a89be356b836855ba9d3c308cb61ebbf
William Henrique Cirino <43853142+williamWHC@users.noreply.github.com>
2020-08-05 14:44:06 -0300

    Create new project
    Create new project ASP NET CORE WEB API MVC

---
+++ MimicAPI/Models/WordModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MimicAPI.Models
+{
+    public class WordModels
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public int ponctuation { get; set; } //pontuacao
+        //verifica se a plalavra esta ativa
+        public bool active { get; set; }    //ativo
+        //data da criação da palavra
+        public DateTime servant { get; set; } //criado
+        //data da atualização 
+        public DateTime updated { get; set; } //atualizar
+    }
+}
